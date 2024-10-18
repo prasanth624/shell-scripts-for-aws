5d6a778338e2ecf53d0b1e648c08942ba6e9a5e1
root <root@localhost.localdomain>
2024-10-18 16:01:17 +0530

    updated unattached-ebs-volumes.sh

--- unattached-ebs-volumes.sh
+++ unattached-ebs-volumes.sh
@@ -3,9 +3,9 @@
 display_region_options() {
     echo "Select the AWS region to find unattached EBS volumes:"
     echo "1) us-east-1"
-    echo "2) us-west-1"
-    echo "3) eu-west-1"
-    echo "4) ap-southeast-1"
+    echo "2) us-east-2"
+    echo "3) us-west-1"
+    echo "4) us-west-2"
     echo "5) Enter a custom region"
     echo "6) Exit"
 }
@@ -18,7 +18,7 @@ while true; do
         2) region="us-east-2"; break ;;
         3) region="us-west-1"; break ;;
         4) region="us-west-2"; break ;;
-        5) read -p "Enter the custom region (e.g., us-east-2): " region; break ;;
+        5) read -p "Enter the custom region (e.g., eu-west-1): " region; break ;;
         6) echo "Exiting."; exit 0 ;;
         *) echo "Invalid choice. Please select a valid option." ;;
     esac
@@ -26,12 +26,15 @@ done
 
 echo "Finding unattached EBS volumes in region $region..."
 
-unattached_volumes=$(aws ec2 describe-volumes --filters Name=status,Values=available --query "Volumes[*].[VolumeId,Size,State,CreateTime]" --output table --region "$region")
+unattached_volumes=$(aws ec2 describe-volumes --filters Name=status,Values=available --query "Volumes[*].[VolumeId,Size,State,CreateTime]" --output table --region "$region" 2>&1)
 
-if [[ -z "$unattached_volumes" ]]; then
-    echo "No unattached EBS volumes found in region $region."
+if [[ $? -ne 0 ]]; then
+    echo "Error: $unattached_volumes"
 else
-    echo "Unattached EBS Volumes in region $region:"
-    echo "$unattached_volumes"
+   if [[ -z "$unattached_volumes" ]]; then
+       echo "No unattached EBS volumes found in region $region."
+   else
+       echo "Unattached EBS Volumes in region $region:"
+       echo "$unattached_volumes"
+   fi
 fi
-
